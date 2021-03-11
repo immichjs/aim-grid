1b7fb88edaed0ee885bc932b0ab63b7f67164d07
Michel França <fr.mich17@gmail.com>
2021-03-11 03:53:06 -0300

    Alinhamento com grid

--- src/script.js
+++ src/script.js
@@ -1,41 +1,42 @@
 const area = document.getElementById('area')
-const score = document.getElementById('score')
+// const score = document.getElementById('score')
 
+let col, row
+
+function gridGen () {
+    col = Math.random() * (4 + 4) - 4
+    row = Math.random() * (4 + 4) - 4
+
+    if (col <= 0 || row <= 0) {
+        gridGen()
+    }
+}
 
 function randomCircles () {
-    let positionTop = Math.floor(Math.random() * (538 * 538) % 538)
-    let positionLeft = Math.floor(Math.random() * (538 * 538) % 538)
-
-    console.log(positionTop, positionLeft)
-    
     const circle = document.createElement('div')
     circle.setAttribute('class', 'circle')
     circle.setAttribute('id', 'circle')
     circle.setAttribute('draggrable', 'false')
-    circle.style.top = positionTop + 'px'
-    circle.style.left = positionLeft + 'px'
+    
+    gridGen()
+    circle.style.gridColumn = Math.ceil(col)
+    circle.style.gridRow = Math.ceil(row)
 
     area.appendChild(circle)
 
     circle.onclick = () => {
         area.removeChild(circle)
+        console.log('Click efetuado')
         randomCircles()
-        scorePoints()
     }
+
 }
 
-let currentValue = 0
-function scorePoints () {
-    currentValue += 1
-    console.log(currentValue)
-    score.innerText = currentValue
-}
 randomCircles()
 randomCircles()
 randomCircles()
 
-setTimeout(function () {
-    alert("Acabou o tempo, seu score foi: " + currentValue)
-    currentValue = 0
-    score.innerHTML = currentValue
-}, 30000);
+// setTimeout(function () {
+//     alert("Acabou o tempo, seu score foi: " + currentValue)
+//     currentValue = 0
+// }, 30000);
