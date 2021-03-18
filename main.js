aaea3ff716e0d385a316b5cb31e86f330d12e02b
Michel França <fr.mich17@gmail.com>
2021-03-18 11:51:17 -0300

    Remoção do menu padrão do Electron

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
     let mainWindow = new BrowserWindow({
-        width: 800,
+        width: 700,
         height: 700,
         icon: __dirname + '/src/img/osu-icon.png',
         resizable: false
@@ -11,6 +11,7 @@ function createWindow () {
 
     mainWindow.loadFile(__dirname + '/src/index.html')
     mainWindow.on('closed', () => mainWindow = null)
+    mainWindow.setMenuBarVisibility(false);
 }
 
 app.on('ready', createWindow)
